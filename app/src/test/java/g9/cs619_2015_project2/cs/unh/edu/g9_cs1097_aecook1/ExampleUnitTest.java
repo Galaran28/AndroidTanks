662244926c3f13d06ff9511dc6c47e04c60ac910
Chris Sleys <chris.sleys@gmail.com>
2015-10-27 13:56:36 -0400

    initial setup of android studio

---
+++ app/src/test/java/g9/cs619_2015_project2/cs/unh/edu/g9_cs1097_aecook1/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package g9.cs619_2015_project2.cs.unh.edu.g9_cs1097_aecook1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
